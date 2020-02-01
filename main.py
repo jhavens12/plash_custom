77de6552ac98f94cd81bf00ef90034277f3d7d06
Jonathan Havens <jhavens12@gmail.com>
2020-02-01 08:35:36 -0500

    Rusti Update

--- main.py
+++ main.py
@@ -13,14 +13,25 @@ app = Flask('Plash_Custom')
 def index():
     with counter.get_lock():
         counter.value += 1
-
     if (counter.value % 2) == 0:
-        print("Yearly")
-        return render_template('index.html', variable=get_yearly())
+        return render_template('index.html', variable=get_yearly(), sizing=get_size(get_yearly()))
     else:
-        print("Total")
-        return render_template('index.html', variable=get_total())
+        return render_template('index.html', variable=get_total(), sizing=get_size(get_total()))
 
+def get_size(variable):
+    length = len(variable)
+    if length > 35:
+        print("Returning small")
+        return "small_VW"
+    elif length > 30:
+        print("Returning medium")
+        return "medium_VW"
+    elif length > 20:
+        print("Returning large")
+        return "large_VW"
+    else:
+        print("Returning xlarge")
+        return "xlarge_VW"
 
 def get_yearly():
     url = "https://smashrun.com/jonamerica/overview/2020"
@@ -41,7 +52,6 @@ def get_total():
     result = soup.find('div', { "class" : "data distance-text" })
 
     return result.text.upper()
-
 
 if __name__ == '__main__':
     app.run(port=5000)
